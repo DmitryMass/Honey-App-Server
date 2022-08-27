347ec7dd481436782d391a3c6b835e2776ce94d0
Dmitry Moskalenko <dimamikron41@gmail.com>
2022-08-27 22:35:39 +0300

    change version

--- src/db.mjs
+++ src/db.mjs
@@ -2,22 +2,22 @@ import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize('honey-server', 'postgres', 'postgres', {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
+// const sequelize = new Sequelize('honey-server', 'postgres', 'postgres', {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+// });
+
+// export default sequelize;
+
+const { DATABASE_URL } = process.env;
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
-
-// const { DATABASE_URL } = process.env;
-// const sequelizeDataBase = new Sequelize(DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
-
-// export default sequelizeDataBase;
